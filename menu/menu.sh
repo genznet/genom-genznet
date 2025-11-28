d2e8e1839ab1889fb69134ca51072535b1ab42cc
genznet <167752950+genznet@users.noreply.github.com>
2025-11-28 17:45:59 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -384,10 +384,10 @@ echo -e "$COLOR1╰════════════════════�
 
 echo -e "$COLOR1╭═══════════════════════════════════════════════════╮${NC}"
 echo -e "$COLOR1│ $NC ${WH}Versi Script  ${COLOR1}: ${WH}$(cat /opt/.ver) Latest Version${NC}$COLOR1"
-echo -e "$COLOR1│ $NC ${WH}Author        ${COLOR1}: ${WH}GENZNET VPN STORE | ${NC}$COLOR1"
-echo -e "$COLOR1│ $NC ${WH}Client        ${COLOR1}: ${WH}$client_name | ${NC}"
-echo -e "$COLOR1│ $NC ${WH}Sisa Hari     ${COLOR1}: ${WH}$days_remaining hari | ${NC}"
-echo -e "$COLOR1│ $NC ${WH}Expire Date   ${COLOR1}: ${WH}$exp_date | ${NC}"
+echo -e "$COLOR1│ $NC ${WH}Author        ${COLOR1}: ${WH}GENZNET VPN STORE${NC}$COLOR1"
+echo -e "$COLOR1│ $NC ${WH}Client        ${COLOR1}: ${WH}$client_name${NC}"
+echo -e "$COLOR1│ $NC ${WH}Sisa Hari     ${COLOR1}: ${WH}$days_remaining hari${NC}"
+echo -e "$COLOR1│ $NC ${WH}Expire Date   ${COLOR1}: ${WH}$exp_date${NC}"
 echo -e "$COLOR1╰═══════════════════════════════════════════════════╯${NC}"
 echo -e ""
 echo -ne " ${WH}Select menu ${COLOR1}: ${WH}"; read opt
