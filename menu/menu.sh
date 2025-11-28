aa37f1b5cb40b4897a94a017f8e1f00061c66c1d
genznet <167752950+genznet@users.noreply.github.com>
2025-11-28 20:24:02 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -369,12 +369,12 @@ echo -e "$COLOR1│ ${WH}[${COLOR1}04${WH}]${NC} ${COLOR1}• ${WH}TRJAN    ${WH
 echo -e "$COLOR1│ ${WH}[${COLOR1}05${WH}]${NC} ${COLOR1}• ${WH}BACKUP   ${WH}[${COLOR1}Menu${WH}]     ${WH}[${COLOR1}10${WH}]${NC} ${COLOR1}• ${WH}SETTING  ${WH}[${COLOR1}Menu${WH}]$COLOR1 │ $NC"
 echo -e "$COLOR1│                                                   $COLOR1│ $NC"
 echo -e "${GREEN}╭═══════${GREEN}══════ • ${GREEN}GENZNET VPN STORE${GREEN} • ${GREEN}═══════════════╮${NC}"
-echo -e "${GREEN}│PILIH NOMOR${NC} ${CYAN}[11] ${WH}🌐 WILDCARD CLOUDFLARE 🌐        ${GREEN} │${NC}"
-echo -e "${GREEN}│PILIH NOMOR${NC} ${CYAN}[12] ${WH}🔍 MELIHAT USER YANG ONLINE 🔍  ${GREEN}  │${NC}"
+echo -e "${GREEN}│${ORANGE}PILIH NOMOR${NC} ${CYAN}[11] ${WH}🌐 WILDCARD CLOUDFLARE 🌐        ${GREEN} │${NC}"
+echo -e "${GREEN}│${ORANGE}PILIH NOMOR${NC} ${CYAN}[12] ${WH}🔍 MELIHAT USER YANG ONLINE 🔍  ${GREEN}  │${NC}"
 echo -e "${GREEN}╰══${GREEN}══════════════════${GREEN}═════════${GREEN}══════════════════════${GREEN}╯${NC}"
 
 echo -e "${RED}╭═══════${RED}══════ • ${RED}GENOM SCRIPT PRO${RED} • ${RED}════════════════╮${NC}"
-echo -e "${RED}│PILIH NOMOR${NC} ${CYAN}[15]    ${WH}🧬 AKSESS GENOM PRO ADMIN 🧬 ${NC} ${RED} │"
+echo -e "${RED}│${ORANGE}PILIH NOMOR${NC} ${CYAN}[15]    ${WH}🧬 AKSESS GENOM PRO ADMIN 🧬 ${NC} ${RED} │"
 echo -e "${RED}╰══${RED}════════════════${RED}═════════${RED}════════════════════════${RED}╯${NC}"
 
 echo -e "$COLOR1╭═══════════════════════════════════════════════════╮${NC}"
